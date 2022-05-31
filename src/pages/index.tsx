5f4ca72089184c63ce680d8af01c60b413b11fa8
adrian <aapodaca@gmail.com>
2022-05-31 15:41:27 -0700

    update state set initial and store.getState"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,55 @@
-import Link from 'next/link'
-import Layout from '../../components/Layout'
+import { useState, useEffect } from 'react';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../app/hooks';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from '../features/counter/counterSlice';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+import {store}from '../app/store'
+const IndexPage:React.FC = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+  const [stateSet, setStateSet ] = useState(false)
+  useEffect(() => {
+    console.log(window)
 
-export default IndexPage
+    if (window && !stateSet) {
+      window.getState = store.getState
+      setStateSet(true)
+    }
+  }, [store])
+
+  return (
+    <>
+      <h1>Welcome to the greatest app in the world!</h1>
+      <h2>
+        The current number is
+        {count}
+      </h2>
+      <div>
+        <input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(Number(e.target.value))}
+          type="number"
+        />
+        <button
+          onClick={() =>        dispatch(incrementByAmount(Number(incrementAmount)))}
+        >
+          Increment by amount
+        </button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        <button onClick={() => dispatch(increment())}>Increment by 1</button>
+      </div>
+    </>
+  );
+};
+
+export default IndexPage;
